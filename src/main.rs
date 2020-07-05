6f128df0f1ef3169d2f8fbf5d7ae0bbf94937fd5
litttley <617914118@qq.com>
2020-07-05 18:23:25 +0800

    1.创建mysql线程池

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
 use actix_web::{get, middleware, web, App, HttpRequest, HttpResponse, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+use dotenv::dotenv;
+
+
+
+
+mod controller;
+mod config;
+use controller::controller_search;
+use crate::config::init_db;
+use crate::config::init_db::{MysqlPool};
+
+
+
+type DbPool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
 
 #[get("/resource1/{name}/index.html")]
 async fn index(req: HttpRequest, name: web::Path<String>) -> String {
@@ -21,8 +40,16 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
     env_logger::init();
 
-    HttpServer::new(|| {
+    /*let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<MysqlConnection>::new(connspec);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");*/
+
+  let mysqlPool   =  init_db::connect();
+    HttpServer::new(move || {
         App::new()
+            .data(mysqlPool.clone())
             .wrap(middleware::DefaultHeaders::new().header("X-Version", "0.2"))
             .wrap(middleware::Compress::default())
             .wrap(middleware::Logger::default())
@@ -39,6 +66,7 @@ async fn main() -> std::io::Result<()> {
                     .route(web::get().to(index_async)),
             )
             .service(web::resource("/test1.html").to(|| async { "Test\r\n" }))
+           .service(controller_search::no_params)
     })
         .bind("127.0.0.1:8080")?
         .workers(1)
