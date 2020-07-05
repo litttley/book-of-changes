1a3e2807d070093a83dc0aaa091d876a0f5e1677
litttley <617914118@qq.com>
2020-07-05 21:44:00 +0800

    录结构调整

--- src/entity/user_entity.rs
+++
@@ -1,9 +0,0 @@
-use crate::schema::user;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Queryable, Insertable)]
-#[table_name = "user"]
-pub struct User {
-    pub userid: i32,
-    pub username: String,
-}
